10c2cc716b482bb22f1afb5017d6ee7f6f6b7f14
ShyGuyAnimations <macjam2007@gmail.com>
2025-12-20 16:23:50 -0500

    unsuccessfully testing more robust charactersystem

---
+++ waka/TrueChar.gd
@@ -0,0 +1,10 @@
+extends Resource
+
+class_name TrueChar
+@export var Name = "Test"
+@export var pretned_i_put_the_stats : int
+@export var actionList : Array 
+@export var sprites = "path" 
+"one sec lemme figure out what packed scene is, might be better for storing character info"
+"ok so research is going somewhere probably but theres no real info about how to have an animation thingy tied to a resource, it probably works with packedscene hopefully"
+"a part of me is resisting the actually helpful urge to just sit down and figure it out myself"
